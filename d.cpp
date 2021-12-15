0594505a225c70a45d7fce62958181043886147a
Sucheol.J <57341121+sudo-G41@users.noreply.github.com>
2021-12-16 01:07:48 +0900

    수정 완료!!
    1. 대각선 이동중 이미 채워진 곳을 만나면 아래로 내려가게 수정
    2. 출력부 '￦n'이 int값으로 10인 점을 이용하여 x=0이면서 줄바꿈 하도록 수정

--- d.cpp
+++ d.cpp
@@ -16,19 +16,13 @@ int main(){
 		else{arr.push_back(a);}
 	}
 	int size = n*n+1;
-	for(int x=n/2,y=0,num=1;num<size;num++){
-		//cout<<"("<<x<<","<<y<<")"<<' ';
-		arr[QWER(y,n)][QWER(x,n)]=arr[y][x]==0?num:num-(0*(y=n-1));
-		print(arr,n);
-		//cout<<"("<<x<<","<<y<<")"<<'\n';
-		cout<<'\n';
-	}
+	for(int x=n/2,y=0,num=1;num<size;num++){arr[QWER(y,n)][QWER(x,n)]=arr[y][x]==0?num:num-(0*(y=(y+2)%n)*(x=++x%n));}
 	print(arr,n);
 	return 0;
 }
 void print(vector<vector<int>>arr,int n){
 	for(int x=0,y=0;y<n;y+=x==0?1:0){
 		if(x<n){cout<<arr[y][x++]<<' ';}
-		else{cout<<'\n'<<(char)(x=0);}
+		else{cout<<(char)(10+(x=0));}
 	}
 }
