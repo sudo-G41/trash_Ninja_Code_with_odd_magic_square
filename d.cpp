222dbdd64dd822c3e5be01b4db158e9eee22e85e
sudo-G41 <usa7372@gmail.com>
2021-12-15 22:47:02 +0900

    Trash Code!

---
+++ d.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#define QWER(N,n) ((N)>0?1+((N)=(N)-1):0+(0*((N)=((n)-1))))
+
+using namespace std;
+
+void print(vector<vector<int>>arr,int n);
+
+int main(){
+	int n;
+	cin>>n;
+	vector<vector<int>>arr;
+	vector<int>a;
+	for(int i=0,j=0;i<n;i+=j<n?0*j++:1){
+		if(j<n){a.push_back(0);}
+		else{arr.push_back(a);}
+	}
+	int size = n*n+1;
+	for(int x=n/2,y=0,num=1;num<size;num++){
+		//cout<<"("<<x<<","<<y<<")"<<' ';
+		arr[QWER(y,n)][QWER(x,n)]=arr[y][x]==0?num:num-(0*(y=n-1));
+		print(arr,n);
+		//cout<<"("<<x<<","<<y<<")"<<'\n';
+		cout<<'\n';
+	}
+	print(arr,n);
+	return 0;
+}
+void print(vector<vector<int>>arr,int n){
+	for(int x=0,y=0;y<n;y+=x==0?1:0){
+		if(x<n){cout<<arr[y][x++]<<' ';}
+		else{cout<<'\n'<<(char)(x=0);}
+	}
+}
